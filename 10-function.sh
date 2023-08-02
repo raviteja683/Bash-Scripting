e59bac93d1c2adacd67595c9d742750d1c72959a
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-03 00:16:17 +0530

    Auto Push

--- 10-function.sh
+++ 10-function.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 rakesh(){
-    echo -e "Hi Ravi, I'm in Warangal!! \n Where are you now?"
+    echo -e "Hi Ravi, I'm in Warangal!! \n you can find the details below"
+    echo "Number of sessions opened are $(who | wc -l)"
+    echo "Todays date is $(date +%F)"
+    echo "Avg CPU Utilization in last 5 minutes $(uptime | awk -F : '{print $NF}' | awk -F , '{print $2}')"
 }
 ravi(){
-    echo "Hi Rakesh,Where are you now?"
+    echo "Hi Rakesh,Where are you now? and some stats about this VM"
     sleep 5
     rakesh
 }
 ravi
+
+
