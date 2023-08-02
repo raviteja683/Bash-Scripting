5a3485dd8571c7ecdf721e5a6a35988faeb61ea4
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-02 12:28:03 +0530

    print echo message

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,1 +1,9 @@
-Sample.sh file
+#!/bin/bash
+
+# Shebang Notation: This tells the sciprt with intreprator this scipt has to be executed
+# it should be in 1st line
+
+echo 'welcome to bash scripting'
+echo Without quotes
+echo 'Hello Ravi'
+echo 'How are you doing today!'
