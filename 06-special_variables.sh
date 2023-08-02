671f6f7cc5744f4138dbe3714f8f7c1baa5d3dbf
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-02 23:41:47 +0530

    Auto Push

---
+++ 06-special_variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+a=10
+b=20
+echo a values is : a
+echo The current execution script is : $0
+echo First alphabet is : $1
+echo First alphabet is : $2
+echo First alphabet is : $3
+echo First alphabet is : $4
+echo First alphabet is : $5
+echo First alphabet is : $6
+echo First alphabet is : $7
+echo First alphabet is : $8
+echo First alphabet is : $9
+
