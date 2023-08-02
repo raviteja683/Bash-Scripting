f1869a12eb1a3c572fd0e30a84ba4a857500a2b8
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-02 15:01:49 +0530

    Auto Push

---
+++ 02-escapeSequence.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Shebang Notation: This tells the sciprt with intreprator this scipt has to be executed
+# it should be in 1st line
+
+echo 'welcome to bash scripting..'
+echo 'Hello Ravi...'
+echo line1
+echo line2
+# Whenever we use esacpe character \n \t etc then use -e (escape)
+echo -e "line3\nline4"
+echo -e 'line5\nline6'
+# \n : new line /t : tab space
+echo -e "Hi\tthis is\tRavi\nfrom Vizianagaram"
