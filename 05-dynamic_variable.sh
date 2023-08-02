5d00d79a039e6f91203724b14f587df45a36c067
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-02 23:35:02 +0530

    Auto Push

--- 05-dynamic_variable.sh
+++ 05-dynamic_variable.sh
@@ -2,4 +2,6 @@
 #print current date
 #DATE=$(date +%F)
 DATE=$(date +%F)
+SESSION_COUNT=$(who | wc -l)
 echo -e "today's date is \e[43;32m $DATE \e[0m"
+echo -e "No. of session count is \e[43;32m $SESSION_COUNT \e[0m"
