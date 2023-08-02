7ea2c481b8766ae5296a91e6cb52cfef322752c6
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-02 12:48:44 +0530

    Auto Push

--- 01-sample.sh
+++ 01-sample.sh
@@ -3,6 +3,6 @@
 # Shebang Notation: This tells the sciprt with intreprator this scipt has to be executed
 # it should be in 1st line
 
-echo 'welcome to bash scripting\nfor automation'
+echo 'welcome to bash scripting'
 echo 'Hello Ravi'
 echo 'How are you doing today!'
