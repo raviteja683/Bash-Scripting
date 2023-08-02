1821751c344cdfe129f80fe1fc0d8c6c3bb94846
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-02 23:52:22 +0530

    Auto Push

---
+++ 07-exitCodes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+
+# Each and every action in linux will have a exit code
+# 0 to 255 is the range of the codes 
+# Among all,  0  represents action completed successfull .
+# Anything in betwenn 1 to 255 represents either partial succes, parital failure , complete failure
