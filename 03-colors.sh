9a264502154e67fa0308cc6fee41c825feaf02c4
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-02 15:20:33 +0530

    Auto Push

--- 03-colors.sh
+++ 03-colors.sh
@@ -2,4 +2,4 @@
 
 echo -e "\e[33m Iam Printing Yellow Color \e[0m"
 echo -e "\e[34m Iam Printing another coloer \e[0m"
-echo -e "\e[33;34m I am printing both colors \e[0m"
+echo -e "\e[43;34m I am printing both colors \e[0m"
