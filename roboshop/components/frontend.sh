e2507cd55d1fb7f58efe8f7f31ad63f7f6e2f111
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 20:34:55 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -15,32 +15,20 @@ stat() {
 }
 echo -n "Frontend (nginx) installation: "
 yum install nginx80 -y  &>> /tmp/frontend.log
-# if [ $? -eq 0 ]; then
-#     echo -e "\e[32m SUCCESS!! \e[0m"
-# else
-#     echo -e "\e[31m FAILED \e[0m"
-#     exit 2
-# fi
 stat $?
-# systemctl enable nginx
-# systemctl start nginx &>> /tmp/frontend.log
-# echo -n "nginx start : "
-# if [ $? -eq 0 ]; then
-#     echo -e "\e[32m SUCCESS!! \e[0m"
-# else
-#     echo -e "\e[31m FAILED!! \e[0m"
-# fi
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> /tmp/frontend.log
-# echo -n "frontend components downloaded : "
-# if [ $? -eq 0 ]; then
-#     echo -e "\e[32m SUCCESS!! \e[0m"
-# else
-#     echo -e "\e[31m FAILED!! \e[0m"
-# fi
-# cd /usr/share/nginx/html
-# rm -rf * &>> /tmp/frontend.log
-# unzip /tmp/frontend.zip &>> /tmp/frontend.log
-# mv frontend-main/* . &>> /tmp/frontend.log
-# mv static/* . &>> /tmp/frontend.log
-# rm -rf frontend-main README.md &>> /tmp/frontend.log
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/frontend.log
+
+
+systemctl enable nginx
+echo -n "nginx start : "
+systemctl start nginx &>> /tmp/frontend.log
+stat $?
+echo -n "frontend components downloaded : "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> /tmp/frontend.log
+stat $?
+cd /usr/share/nginx/html
+rm -rf * &>> /tmp/frontend.log
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
+mv frontend-main/* . &>> /tmp/frontend.log
+mv static/* . &>> /tmp/frontend.log
+rm -rf frontend-main README.md &>> /tmp/frontend.log
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/frontend.log
