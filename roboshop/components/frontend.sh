f23a31eeecbef7f445d14999971378362bc4e635
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 19:55:44 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -18,6 +18,7 @@ if [ $1 -eq 0 ]; then
     echo -e "\e[32m SUCCESS!! \e[0m"
     else
         echo -e "\e[31m FAILED \e[0m"
+        exit 1
     fi
 systemctl enable nginx
 systemctl start nginx &>> /tmp/frontend.log
