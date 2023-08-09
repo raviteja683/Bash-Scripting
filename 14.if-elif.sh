7c94dd0cadf37d4de0fc9e822b3c41bd70fa4753
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 14:37:04 +0530

    Auto Push

--- 14.if-elif.sh
+++ 14.if-elif.sh
@@ -5,7 +5,9 @@ if [ $ACTION == "start" ];
 then
     echo -e "\e[33m Payment start is ready\e[0m"
     exit 0
-
+elif  
+    echo -e "\e[34m Payment restart is ready\e[0m"
+    exit 1
 else
     echo  -e "\e[31m Payment argument is different \e[0m"
     exit 1
