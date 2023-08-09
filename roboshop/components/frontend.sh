12f6264012b6295280fb95f97cab05d25187e785
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 19:36:36 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 echo "configuring frontend:"
 USER_ID=$(id -u)
-echo "user id is : $USER_ID"
 if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[33m You need install frontend as root user!!\e[0m "
     exit 1
@@ -9,12 +8,17 @@ fi
 echo -e "\e[32m installing nginx.....please wait \e[0m"
 yum install nginx -y  &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then
-    echo -e "\e[32m ngnix installed successfully!! \e[0m"
+    echo -e "\e[32m SUCCESS!! \e[0m"
 else
-    echo -e "\e[31m ngnix installation failed \e[0m"
+    echo -e "\e[31m FAILED \e[0m"
 fi
-# systemctl enable nginx
-# systemctl start nginx
+systemctl enable nginx
+systemctl start nginx
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS!! \e[0m"
+else
+    echo -e "\e[31m FAILED!! \e[0m"
+fi
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 # cd /usr/share/nginx/html
 # rm -rf *
