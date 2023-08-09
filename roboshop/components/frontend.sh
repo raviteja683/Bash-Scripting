b11f9cae214a0e9871e739b54e92ba6fb3a46cb6
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 19:15:00 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,6 +8,12 @@ if [ $USER_ID -ne 0 ] ; then
 fi
 echo -e "\e[32m installing nginx.....please wait \e[0m"
 yum install nginx -y &>> /tmp/frontend.log
+if [$? -eq 0]; then
+    echo -e "\e[32m ngnix installed successfully!! \e[0m"
+
+else
+    echo -e "\e[31m ngnix installation failed \e[0m"
+fi
 # systemctl enable nginx
 # systemctl start nginx
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
