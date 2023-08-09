d1bc8920e3ef6cd0e213a87e8aa69b2eba4f7bbd
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 22:30:44 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,8 +1,9 @@
 #/bin/bash
-echo "configuring frontend:"
+COMPONENT=frontend
+echo "configuring ${COMPONENT}:"
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ] ; then
-    echo -e "\e[33m You need install frontend as root user!!\e[0m "
+    echo -e "\e[33m You need install ${COMPONENT} as root user!!\e[0m "
     exit 1
 fi
 stat() {
@@ -13,26 +14,26 @@ stat() {
         exit 2
     fi
 }
-echo -n "Frontend (nginx) installation: "
-yum install nginx -y  &>> /tmp/frontend.log
+echo -n "${COMPONENT} (nginx) installation: "
+yum install nginx -y  &>> /tmp/${COMPONENT}.log
 stat $?
 systemctl enable nginx
 echo -n "nginx start : "
-systemctl start nginx &>> /tmp/frontend.log
+systemctl start nginx &>> /tmp/${COMPONENT}.log
 stat $?
-echo -n "frontend components downloaded : "
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> /tmp/frontend.log
+echo -n "${COMPONENT} components downloaded : "
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" &>> /tmp/${COMPONENT}.log
 stat $?
 cd /usr/share/nginx/html
-rm -rf * &>> /tmp/frontend.log
-echo -n "Extracting Frontend:"
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
+rm -rf * &>> /tmp/${COMPONENT}.log
+echo -n "Extracting ${COMPONENT}: "
+unzip /tmp/${COMPONENT}.zip &>> /tmp/${COMPONENT}.log
 stat $?
-mv frontend-main/* . 
+mv ${COMPONENT}-main/* . 
 mv static/* . 
-rm -rf frontend-main README.md &>> /tmp/frontend.log
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/frontend.log
-echo -n "Restarting Frontend:"
-systemctl daemon-reload     &>>  /tmp/frontend.log
-systemctl restart nginx     &>>  /tmp/frontend.log
+rm -rf ${COMPONENT}-main README.md &>> /tmp/${COMPONENT}.log
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/${COMPONENT}.log
+echo -n "Restarting ${COMPONENT}:"
+systemctl daemon-reload     &>>  /tmp/${COMPONENT}.log
+systemctl restart nginx     &>>  /tmp/${COMPONENT}.log
 stat $?
