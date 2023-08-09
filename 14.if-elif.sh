9e4bbad00020745a133d99ea0d34064f4bad6df1
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 14:35:53 +0530

    Auto Push

--- 14.if-elif.sh
+++ 14.if-elif.sh
@@ -3,11 +3,11 @@ ACTION=$1
 echo "if else demo"
 if [ $ACTION == "start" ];
 then
-    echo Payment start is ready
+    echo -e "\e[33m Payment start is ready\e[0m"
     exit 0
 
 else
-    echo Payment argument is different
+    echo  -e "\e[31m Payment argument is different \e[0m"
     exit 1
 fi
 
