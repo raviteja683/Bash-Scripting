258d55fab856eaa812670552b1c3f33dbcc801bc
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 13:58:44 +0530

    Auto Push

--- 12.conditions.sh
+++ 12.conditions.sh
@@ -5,9 +5,9 @@ case $ACTION in
         echo -e "Payment \e[32m start \e[0m service"
     ;;
     stop)
-        echo "Payment stop service"
+        echo -e "\e[33m Payment stop service \e[0m"
     ;;
     *)
-        echo -e "You should provide stop / start as a agurment \n ex: bash scriptname.sh start"
+        echo -e "You should provide stop / start as a agurment \n \e[34m ex: bash scriptname.sh start \e[0m"
     ;;
 esac
