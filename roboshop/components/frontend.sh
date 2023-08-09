7de230adb9fc1207a3395d6330fb5d2e42e19756
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 20:09:22 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,7 +5,7 @@ if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[33m You need install frontend as root user!!\e[0m "
     exit 1
 fi
-status() {
+stat() {
     if [ $1 -eq 0 ]; then 
         echo -e "\e[32m success \e[0m"
     else 
@@ -15,20 +15,14 @@ status() {
 }
 yum install nginxdffgfkgjfd -y  &>> /tmp/frontend.log
 echo -n "Frontend (nginx) installation : "
-#status $?
-if [ $1 -eq 0 ]; then 
-        echo -e "\e[32m success \e[0m"
-    else 
-        echo -e "\e[31m failure \e[0m"
-        exit 2
-    fi
+stat $?
 systemctl enable nginx
 systemctl start nginx &>> /tmp/frontend.log
 echo -n "nginx start : "
-status $?
+stat $?
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> /tmp/frontend.log
 echo -n "frontend components downloaded : "
-status $?
+stat $?
 cd /usr/share/nginx/html
 rm -rf * &>> /tmp/frontend.log
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
