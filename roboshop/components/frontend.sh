2cf68ace5badd3a0f17a9ae0cc583993f18e76e1
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 20:28:59 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,7 +5,7 @@ if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[33m You need install frontend as root user!!\e[0m "
     exit 1
 fi
-echo -n "Frontend (nginx) installation : "
+echo -n "Frontend (nginx) installation: "
 yum install nginx80 -y  
 if [ $? -eq 0 ]; then
     echo -e "\e[32m SUCCESS!! \e[0m"
