bc63635c71028614072c61d814e495def28fbca5
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 19:02:22 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 echo "configuring frontend:"
 USER_ID= $(id -u)
-if [ $USER_ID -ne 0 ];then
+if [ $USER_ID -ne 0 ] ;then
     echo "You need install frontend as root user!! "
     exit 1
 fi
