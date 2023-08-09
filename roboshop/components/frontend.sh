b99bf23cf1a3d8619c2dea725c4cfe361a419fff
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-09 20:22:13 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -13,25 +13,25 @@ else
     echo -e "\e[31m FAILED \e[0m"
     exit 2
 fi
-systemctl enable nginx
-systemctl start nginx &>> /tmp/frontend.log
-echo -n "nginx start : "
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS!! \e[0m"
-else
-    echo -e "\e[31m FAILED!! \e[0m"
-fi
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> /tmp/frontend.log
-echo -n "frontend components downloaded : "
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS!! \e[0m"
-else
-    echo -e "\e[31m FAILED!! \e[0m"
-fi
-cd /usr/share/nginx/html
-rm -rf * &>> /tmp/frontend.log
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
-mv frontend-main/* . &>> /tmp/frontend.log
-mv static/* . &>> /tmp/frontend.log
-rm -rf frontend-main README.md &>> /tmp/frontend.log
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/frontend.log
+# systemctl enable nginx
+# systemctl start nginx &>> /tmp/frontend.log
+# echo -n "nginx start : "
+# if [ $? -eq 0 ]; then
+#     echo -e "\e[32m SUCCESS!! \e[0m"
+# else
+#     echo -e "\e[31m FAILED!! \e[0m"
+# fi
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> /tmp/frontend.log
+# echo -n "frontend components downloaded : "
+# if [ $? -eq 0 ]; then
+#     echo -e "\e[32m SUCCESS!! \e[0m"
+# else
+#     echo -e "\e[31m FAILED!! \e[0m"
+# fi
+# cd /usr/share/nginx/html
+# rm -rf * &>> /tmp/frontend.log
+# unzip /tmp/frontend.zip &>> /tmp/frontend.log
+# mv frontend-main/* . &>> /tmp/frontend.log
+# mv static/* . &>> /tmp/frontend.log
+# rm -rf frontend-main README.md &>> /tmp/frontend.log
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/frontend.log
