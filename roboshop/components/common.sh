0a887e41a3b47646f3067300f68f78f8b72bf848
Syam Kumar Vatti <raviteja121683@gmail.com>
2023-08-15 21:47:25 +0530

    Auto Push

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -36,10 +36,6 @@ DOWNLOAD_AND_EXTRACT() {
         curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
         stat $? 
 
-        echo -n "Downloading the ${COMPONENT} : "
-        curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
-        stat $? 
-
         cd /home/${APPUSER}/
         rm -rf ${COMPONENT}     &>> ${LOGFILE}
         unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
@@ -55,7 +51,7 @@ DOWNLOAD_AND_EXTRACT() {
 CONFIG_SVC() {
 
         echo -n "Configuring the ${COMPONENT} system file :"
-        sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service
+        sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/'  -e 's/CARTHOST/cart.roboshop.internal/'  -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service
         mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
         stat $? 
 
@@ -88,4 +84,59 @@ NODEJS() {
         npm install     &>> ${LOGFILE}
         stat $? 
 
+        CONFIG_SVC
+
+}
+
+
+MVN_PACKAGE() {
+        echo -n "Generating the ${COMPONENT} artifacts :"
+        cd /home/${APPUSER}/${COMPONENT}/
+        mvn clean package   &>> ${LOGFILE}
+        mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar
+        stat $?
+}
+
+JAVA() {
+        echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
+
+        echo -n "Installing maven:"
+        yum install maven -y    &>> ${LOGFILE}
+        stat $? 
+
+        CREATE_USER              # calls CREATE_USER function that creates user account.
+
+        DOWNLOAD_AND_EXTRACT     # Downloads and extracts the components
+
+        MVN_PACKAGE
+
+        CONFIG_SVC
+
+}
+
+
+PYTHON() {
+        echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
+
+        echo -n "Installing python:"
+        yum install python36 gcc python3-devel -y &>> ${LOGFILE}
+        stat $? 
+
+        CREATE_USER              # calls CREATE_USER function that creates user account.
+
+        DOWNLOAD_AND_EXTRACT     # Downloads and extracts the components
+
+        echo -n "Generating the artifacts"
+        cd /home/${APPUSER}/${COMPONENT}/ 
+        pip3 install -r requirements.txt    &>> ${LOGFILE} 
+        stat $?
+
+        USERID=$(id -u roboshop)
+        GROUPID=$(id -g roboshop)
+
+        echo -n "Updating the uid and gid in the ${COMPONENT}.ini file"
+        sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}" /home/${APPUSER}/${COMPONENT}/${COMPONENT}.ini
+        stat $?
+
+        CONFIG_SVC
 }
